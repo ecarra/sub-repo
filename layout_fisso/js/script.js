d5a640efea6d7fe0b09e27a7b8616c3978e2f145
ecarra <34311119+ecarra@users.noreply.github.com>
2017-12-07 19:06:14 +0100

    Add files via upload

---
+++ layout_fisso/js/script.js
@@ -0,0 +1,35 @@
+
+        $(document).ready(function() {
+        /* fancybox plugin */
+        $("a.portfolio-box–1").fancybox({
+			transitionIn: "elastic",
+			transitionOut: "elastic",
+			speedIn: 5000,
+			speedOut: 5000,
+			speedIn: 2,
+			speddOut: 2
+			
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/* jquery cycle */
+		$("#focus2").cycle({
+			fx: 'scrollHorz',
+			speed: 1000,
+			timeout: 2000,
+			next: "#next",
+			prev: "#prev"
+		
+		    });
+		
+		});
+        
